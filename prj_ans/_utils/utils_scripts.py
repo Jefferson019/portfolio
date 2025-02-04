5272756bb0a6bb24c54ad891b32d66b3cf94f11b
jefferson <jeffersonm019@gmail.com>
2025-02-04 00:43:02 -0300

    ajuste para ler arquivos zip

--- prj_ans/_utils/utils_scripts.py
+++ prj_ans/_utils/utils_scripts.py
@@ -1,5 +1,7 @@
 import io 
 from ftplib import FTP
+import zipfile
+import pandas as pd
 
 class SmartFTP(FTP):
     def makepasv(self):
@@ -14,3 +16,27 @@ def get_readfile(ftp, no_file):
 
     download_file.seek(0)
     return download_file 
+
+def list_zip_from_ftp(ftp, zip_filename):
+    # Baixar o arquivo ZIP
+    zip_file = get_readfile(ftp, zip_filename)
+    
+    # Abrir o ZIP diretamente da memória
+    with zipfile.ZipFile(zip_file, "r") as z:
+        file_list = z.namelist()  # Lista de arquivos no ZIP
+        print("Arquivos no ZIP:", file_list)
+
+    return file_list
+
+def process_zip_from_ftp(ftp, list_zip):
+    dataframes = {}
+        
+    # Ler cada arquivo no ZIP e salvar em um dicionário
+    for filename in list_zip:
+        if filename.endswith(".csv"):  # Se for um CSV
+            with z.open(filename) as file:
+                df = pd.read_csv(file, encoding="utf-8")  # Ajuste o encoding se necessário
+                dataframes[filename] = df
+                print(f"Lido: {filename}, {df.shape}")
+
+    return dataframes
