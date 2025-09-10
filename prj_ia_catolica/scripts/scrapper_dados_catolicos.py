78b6712f140453bf1fc4403b2e6f15dd53c2b037
jeffersonm019 <jeffersonm019@gmail.com>
2025-09-10 00:36:43 -0300

    atualizacao e criação do script de vector search

--- prj_ia_catolica/scripts/scrapper_dados_catolicos.py
+++ prj_ia_catolica/scripts/scrapper_dados_catolicos.py
@@ -6,7 +6,7 @@ import spacy
 from langchain_chroma import Chroma
 from langchain_community.embeddings.spacy_embeddings import SpacyEmbeddings
 
-nlp_sm = spacy.load("pt_core_news_sm")
+nlp_sm = spacy.load("pt_core_news_lg")
 
 url = "https://www.vatican.va/archive/cathechism_po/index_new/prima-pagina-cic_po.html"
 response = requests.get(url)
@@ -20,10 +20,16 @@ def get_links(url, soup):
     for link in links:
         href = link.get('href')
         if href:
-            new_url = '/'.join(url.split("/")[:-1])+"/"+href  
-            all_links.append(new_url)
+            if isinstance(href, list):
+                print("List found")
+                all_links.extend(href)
+            else:
+                new_url = '/'.join(url.split("/")[:-1])+"/"+href 
+                all_links.append(new_url)
 
     all_links = list(set(t for t in all_links))
+
+    all_links.sort()
 
     return all_links
 
@@ -32,14 +38,15 @@ def get_all_texts(all_links):
     textos_link = []
 
     for link in all_links:
-        print(link)
+        print("...get link:",link)
         page = requests.get(link)
         soup = BeautifulSoup(page.text, 'html.parser')
 
         texts = soup.find_all('p')
+        titulos = soup.find_all('h3')
 
         textos = [t.text for t in texts]
-        print(len(textos))
+        #print(len(textos))
         dic = {'link':link, 'textos': textos}
 
         textos_link.append(dic)
@@ -52,20 +59,20 @@ def get_embeddings(texto):
     return nlp_sm(texto).vector
 
 
-
-
-
 DEBUG = True
 
 if __name__ == '__main__':
-
+    texts_embeddings = []
     if DEBUG:
         all_links = get_links(url, soup)
         textos = get_all_texts(all_links)
-        texts_embeddings = [get_embeddings(text) for text in [texts['textos'] for texts in textos ]]
 
-        for t in textos:
+        for text in [texts['textos'] for texts in textos[:-1]]:
             
-            print(t['link'])
-            #print(t.find('href'))
+            if not isinstance(text, list):
+                texts_embeddings.append(get_embeddings(text))
+            
+        #for t in textos[:10]:
+        #    print(t)
 
+      
